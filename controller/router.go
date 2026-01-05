1d0e06abb4d62dffabc2a986daba75cfd51cd20e
yixixiawa <yixixiawa@qq.com>
2026-01-05 15:02:34 +0800

    auto commit

--- controller/router.go
+++ controller/router.go
@@ -107,11 +107,18 @@ func InitRoutes(r *gin.Engine) {
 func SetupMiddlewares(r *gin.Engine) {
 	// CORS中间件
 	r.Use(func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
 
-		if c.Request.Method == "OPTIONS" {
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		if method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
